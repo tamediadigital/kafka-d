0a79fb4bb5ef047482c4899faae233b2c459f3da
Piotr Szturmaj <bncrbme@jadamspam.pl>
2015-08-27 16:58:13 +0200

    Consumer queue handling

--- source/kafkad/config.d
+++ source/kafkad/config.d
@@ -7,8 +7,8 @@ struct Configuration {
     int consumerMinBytes = 1;
     /// maximum number of bytes to include in the message set
     int consumerMaxBytes = 1048576;
-    /// maximum number of bytes in the consumer message queue, must be greater or equal to consumerMaxBytes
-    int consumerQueueMaxBytes = 10485760;
+    /// number of queue buffers, each one has size of consumerMaxBytes, must be at least 2
+    int consumerQueueBuffers = 10;
     /// number of retries to perform when refreshing the metadata, 0 = retry infinitely
     int metadataRefreshRetryCount = 3;
     /// time to wait between retries when refreshing the metadata
--- source/kafkad/consumer.d
+++ source/kafkad/consumer.d
@@ -3,11 +3,98 @@
 import kafkad.client;
 import kafkad.connection;
 import kafkad.protocol.fetch;
+import vibe.core.sync;
+import std.container.dlist;
 
 struct Message {
     long offset;
     ubyte[] key;
     ubyte[] value;
+}
+
+struct QueueBuffer {
+    ubyte[] buffer;
+    size_t filled;
+}
+
+class QueueGroup {
+    private {
+        DList!Queue m_queues;
+        TaskMutex m_mutex;
+        TaskCondition m_freeCondition; // notified when there are queues with free buffers
+    }
+
+    void addQueue(Queue queue) {
+        synchronized (m_mutex) {
+            m_queues.insertBack(queue);
+        }
+    }
+
+    void notifyQueuesHaveFreeBuffers() {
+        m_freeCondition.notify;
+    }
+}
+
+class Queue {
+    private {
+        DList!(QueueBuffer*) m_freeBuffers, m_filledBuffers;
+        QueueBuffer* m_lastBuffer;
+        TaskMutex m_mutex;
+        TaskCondition m_filledCondition;
+        QueueGroup m_group;
+    }
+
+    package bool m_fetchPending; // this is updated in the fetch task
+
+    this(in Configuration config, QueueGroup group) {
+        import std.algorithm : max;
+        auto nbufs = max(2, config.consumerQueueBuffers); // at least 2
+        foreach (n; 0 .. nbufs) {
+            auto buf = new ubyte[config.consumerMaxBytes];
+            auto qbuf = new QueueBuffer(buf, 0);
+            m_freeBuffers.insertBack(qbuf);
+        }
+        m_lastBuffer = null;
+        m_mutex = new TaskMutex();
+        m_filledCondition = new TaskCondition(m_mutex);
+        m_group = group;
+    }
+
+    @property auto mutex() { return m_mutex; }
+    @property auto filledCondition() { return m_filledCondition; }
+
+    auto getFreeBuffer() {
+        auto qbuf = m_freeBuffers.front();
+        m_freeBuffers.removeFront();
+        return qbuf;
+    }
+
+    QueueBuffer* waitForFilledBuffer() {
+        synchronized (m_mutex) {
+            if (m_lastBuffer) {
+                // return the last used buffer to the free buffer list
+                m_freeBuffers.insertBack(m_lastBuffer);
+                // notify the fetch task that there are buffer to be filled in
+                // the fetch task will then make a batch request for all queues with free buffers
+                // do not notify the task if there is a pending request (e.g. without a response)
+                if (!m_fetchPending)
+                    m_group.notifyQueuesHaveFreeBuffers();
+            }
+
+            while (m_filledBuffers.empty)
+                m_filledCondition.wait();
+            m_lastBuffer = m_filledBuffers.front;
+            m_filledBuffers.removeFront();
+            return m_lastBuffer;
+        }
+    }
+
+    /*
+    void returnProcessedBuffer(QueueBuffer* buf) {
+        synchronized (m_mutex) {
+            m_freeBuffers.insertBack(buf);
+        }
+    }*/
 }
 
 class Consumer {
@@ -17,6 +104,9 @@ class Consumer {
         int m_partition;
         int m_offset;
         ubyte[] m_msgBuffer;
+        size_t m_filled;
+        TaskCondition m_cond;
+        Queue m_queue;
     }
 
     package {
@@ -29,7 +119,8 @@ class Consumer {
         m_topic = topic;
         m_partition = partition;
         m_offset = offset;
-        //m_msgBuffer = 
+        m_queue = new Queue(client.config);
+        m_cond = new TaskCondition(new TaskMutex);
     }
 
     /// Consumes message from the selected topics and partitions
@@ -41,7 +132,8 @@ class Consumer {
     }+/
 
     Message getMessage() {
-        //m_conn.
+        QueueBuffer* qbuf = m_queue.waitForFilledBuffer();
+        // TODO: parse qbuf data, check crc, and setup key and value slices FOR EACH MESSAGE, also handle last partial msg
         return Message();
     }
 }
